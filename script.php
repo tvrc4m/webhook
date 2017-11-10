49de64d0d75a387fe4ed9e5bc51d94dc7be897bf
xiaoayong <xiaoayong@kanfanews.com>
2017-11-10 11:42:19 +0800

    脚本

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 $title=$_GET['title'];
 $text=$_GET['text'];
-$access_token='699b16574ca2c3c851095fe5555fe5b878a5d96e4303d0a5ce8a1d991e3b1166'
+$access_token='699b16574ca2c3c851095fe5555fe5b878a5d96e4303d0a5ce8a1d991e3b1166';
 
 if(empty($title) || empty($text)) exit('缺少参数');
 
