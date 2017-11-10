8da964b261fea07aaf1167c6269b84c09d703928
xiaoayong <xiaoayong@kanfanews.com>
2017-11-10 11:28:02 +0800

    脚本文件

---
+++ script.php
@@ -0,0 +1,18 @@
+<?php
+
+$title=$_GET['title'];
+$text=$_GET['text'];
+$access_token='699b16574ca2c3c851095fe5555fe5b878a5d96e4303d0a5ce8a1d991e3b1166'
+
+if(empty($title) || empty($text)) exit('缺少参数');
+
+define('ROOT', __DIR__);
+
+include_once(ROOT.'/jenkins/cli.php');
+include_once(ROOT.'/dingtalk/notify.php');
+
+$dingtalk_notify=new DingtalkNotify($access_token);
+
+$dingtalk_notify->deployStart($title,$text);    
+
+print_r($result);
