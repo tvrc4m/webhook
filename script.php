9f249d659400bc2266125230b06cf28b715fe32e
xiaoayong <xiaoayong@kanfanews.com>
2017-11-10 16:37:38 +0800

    钉钉提醒

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 $script=$_GET['script'];
 $ding_array = unserialize($script);
-$access_token='c411aafaaadedc846a9c3aa498e6c38c55434406387cb5e4d555b19dd37d39da';
+$access_token='0066be0fc686e9f11e20e28082f76acf9659e7a1d5b5065455403ae8b536b3a8';
 
 if(empty($ding_array['name']) || empty($ding_array['run_time'])) exit('缺少参数');
 
