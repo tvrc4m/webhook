28ecf42499d1ee34209bd68c125ad2f0b1258cf8
xiaoayong <xiaoayong@kanfanews.com>
2017-11-15 16:17:47 +0800

    xiugai

--- script.php
+++ script.php
@@ -2,7 +2,7 @@
 
 $script=$_GET['script'];
 $ding_array = unserialize($script);
-$access_token='0066be0fc686e9f11e20e28082f76acf9659e7a1d5b5065455403ae8b536b3a8';
+$access_token=$ding_array['access_token'] ? $ding_array['access_token'] : '0066be0fc686e9f11e20e28082f76acf9659e7a1d5b5065455403ae8b536b3a8';
 
 if(empty($ding_array['name']) || empty($ding_array['run_time'])) exit('缺少参数');
 
