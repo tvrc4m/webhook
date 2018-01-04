701f7680151c2697d4e6ab328edc7a2121d5b0e4
System Administrator <root@gizans-MacBook-Pro.local>
2018-01-04 14:15:15 +0800

    添加dev环境负责人通知

---
+++ callback/attention.php
@@ -0,0 +1,40 @@
+<?php
+
+define('ROOT', dirname(__dir__));
+date_default_timezone_set("Asia/Shanghai");
+
+$week=date('w');
+$month=date('m');
+$day=date('d');
+
+// 周六和周日直接返回
+if($week==0 || $week==6) return;
+// 部分节假日
+if($month==2 && in_array($day,[15,16,19,20,21])) return;
+if($month==4 && in_array($day,[5,6,30])) return;
+if($month==5 && in_array($day,[1])) return;
+if($month==6 && in_array($day,[18])) return;
+if($month==9 && in_array($day,[24])) return;
+
+$access_token='699b16574ca2c3c851095fe5555fe5b878a5d96e4303d0a5ce8a1d991e3b1166';
+
+include_once(ROOT.'/dingtalk/notify.php');
+
+$dingtalk_notify=new DingtalkNotify($access_token);
+
+$developers=['郑生齐','肖阿勇','王娟娟','李硕','刘艳梅','武超','许俊帅','魏山'];
+
+$count=count($developers);
+
+$logfile="/var/log/attention.log";
+
+$pre=@file_get_contents($logfile);
+
+$pre=intval($pre);
+
+$current=($count<=$pre+1)?0:$pre+1;
+
+$dingtalk_notify->notifyText('今日DEV环境负责人','负责定位问题,由问题生成者解决: '.$developers[$current]);
+
+file_put_contents($logfile, $current);
+
